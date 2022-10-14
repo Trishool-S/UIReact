36fde028ec034afed04e556caf472c8f3c1f78ff
rajeshshriji <rajesh.shriji11@gmail.com>
2022-10-14 16:27:37 +0530

    add maps marker

--- src/app/container/google-map/index.js
+++ src/app/container/google-map/index.js
@@ -25,6 +25,7 @@
 
 // import './map.scss';
 // import './marker.scss';
+// // import wells from './wells';
 
 // const AnyReactComponent = ({ text }) => (
 //   <div className="marker">
@@ -32,25 +33,25 @@
 //   </div>
 // );
 
-// const WellMap = ({ wells }) => {
+// function WellMap() {
 //   return (
 //     <Box>
 //       <Separator text="Asset Map Overview" />
 //       <Box className="map-container">
 //         <GoogleMapReact
 //           bootstrapURLKeys={{ key: 'AIzaSyAToPYxlf5KrYWm3BA58CmJvdVbPSG-6RE' }}
-//           // defaultCenter={wells[0].geolocation}
+//           initialCenter={{
+//             lat: 23.587961,
+//             lng: 72.369324
+//           }}
 //           defaultZoom={13}
 //           options={{ styles: mapStyles }}
 //         >
-//           {/* {wells.slice(0, 10).map((well) => (
 //             <AnyReactComponent
-//               key={well.uuid}
-//               lat={well.geolocation.lat}
-//               lng={well.geolocation.lng}
+//               lat={23.587961}
+//               lng={72.369324}
 //               text="My Marker"
 //             />
-//           ))} */}
 //         </GoogleMapReact>
 //       </Box>
 //     </Box>
@@ -59,9 +60,11 @@
 
 // export default WellMap;
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import './map.scss';
 import './marker.scss';
+// import Box from '@material-ui/core/Box';
+
 
 const mapStyles = {
   width: '50%',
@@ -72,12 +75,36 @@ const AnyReactComponent = ({ text }) => (
     <div className="marker-pin"> </div>
   </div>
 );
+// const Marker = props => {
+//   return <div className="marker">
+//    <div className="marker-pin"></div>
+//    </div>
+// }
 
 
 export class MapContainer extends Component {
   render() {
     return (
-      <><Map
+      // <>
+      //   <Map
+      //     google={this.props.google}
+      //     zoom={9}
+      //     style={mapStyles}
+      //     initialCenter={{
+      //       lat: 23.587961,
+      //       lng: 72.369324
+      //     }}
+      //     onClick={this.onMapClicked}
+
+      //   />
+      //   <Marker lat={23.586761} lng={72.369949} />
+      //   <AnyReactComponent
+      //     lat={23.587961}
+      //     lng={72.369324}
+      //     text="My Marker"
+      //   />
+      // </>
+      <Map
         google={this.props.google}
         zoom={9}
         style={mapStyles}
@@ -85,13 +112,18 @@ export class MapContainer extends Component {
           lat: 23.587961,
           lng: 72.369324
         }}
-          />
-          <AnyReactComponent
-          lat={23.587961}
-          lng={72.369324}
-          text="My Marker"
-           />
-          </>
+
+        onClick={this.onMapClicked}>
+        <Marker onClick={this.onMarkerClick}
+          name={'Current location'}
+          position={{ lat: 23.587961, lng: 72.369324 }} />
+        <Marker onClick={this.onMarkerClick}
+          name={'Current location'}
+          position={{ lat: 23.51401, lng: 72.182881 }} />
+        <Marker onClick={this.onMarkerClick}
+          name={'Current location'}
+          position={{ lat: 23.033863, lng: 72.585022 }} />
+      </Map>
     );
   }
 }
@@ -99,6 +131,7 @@ export class MapContainer extends Component {
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyAToPYxlf5KrYWm3BA58CmJvdVbPSG-6RE'
 })(MapContainer);
+
 
 
 
