5387393ab442c575261822f5179c908aa1dcb667
patelshivu <patelshivu197@gmail.com>
2022-12-13 11:23:37 +0530

    Backend domain URL Change in the UI code

--- src/app/helper/rest-api/url-schema.js
+++ src/app/helper/rest-api/url-schema.js
@@ -1,7 +1,7 @@
 export const BaseUrl =
   process.env.REACT_APP_ENV === "production"
-    ? "https://a641jo3dda.execute-api.ap-south-1.amazonaws.com/stage2/admin/"
-    : "https://a641jo3dda.execute-api.ap-south-1.amazonaws.com/stage2/admin/";
+    ? "https://2llut6v3gb.execute-api.ap-south-1.amazonaws.com/stage2/admin/"
+    : "https://2llut6v3gb.execute-api.ap-south-1.amazonaws.com/stage2/admin/";
 
 export const auth = {
   login: "authorization/login",
